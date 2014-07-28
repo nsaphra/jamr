0688de8a2991a988044afddacfbd7af729010938
N. Saphra <nsaphra@gmail.com>
2014-07-28 14:49:24 +0200

    DoubleLinkedList now compiles

--- src/JointDecoder/DoubleLinkedList.scala
+++ src/JointDecoder/DoubleLinkedList.scala
@@ -1,74 +1,77 @@
 package edu.cmu.lti.nlp.amr.JointDecoder
 import edu.cmu.lti.nlp.amr._
 
-import scala.collection.mutable.List
+class LinkedNode[A](val data: A, var next: Option[LinkedNode[A]]) {
 
-class DoubleLinkedList[A](list: List[A]) extends Iterable[LinkedNode[A]] {
-
-    class LinkedNode[A](data: A, var next: Option[LinkedNode[A]]) {
-
-        var prev: Option[LinkedNode[A]] = next match {
-            case None => None
-            case Some(n) => {
-                prev_node = n.prev
-                n.prev = this
-                prev_node match {
-                    case None => None
-                    case Some(p) => {
-                        p.next = this
-                        Some(p)
-                    }
+    var prev: Option[LinkedNode[A]] = next match {
+        case Some(n) => {
+            val prev_node = n.prev
+            n.prev = Some(this)
+            prev_node match {
+                case None => None
+                case Some(p) => {
+                    p.next = Some(this)
+                    Some(p)
                 }
             }
         }
+        case None => None
     }
+}
 
-    val tail: Option[LinkedNode[A]] = None
-    var head = list.foldLeft(tail) {
-        (z, f) => LinkedNode[A](f, z)
+class DoubleLinkedList[A](list: List[A]) extends Iterable[LinkedNode[A]] {
+
+    var top: Option[LinkedNode[A]] = list.foldLeft(None.asInstanceOf[Option[LinkedNode[A]]]) {
+        (z, f) => Some(new LinkedNode[A](f, z))
     }
 
     def prepend(data: A) : LinkedNode[A] = {
-        head = LinkedNode[A](data, head)
-        return head
+        top = Some(new LinkedNode[A](data, top))
+        return top.get
     }
 
     def insert(data: A, node: Option[LinkedNode[A]]) : LinkedNode[A] = node match {
-        case None => prepend(A)
-        case Some(n) => {
-            LinkedNode[A](data, n.next)
-        }
+        case Some(n) =>
+            new LinkedNode[A](data, n.next)
+        case None =>
+            prepend(data)
     }
 
-    def remove(node: LinkedNode[A]) : A {
+    def remove(node: LinkedNode[A]) : A = {
         node.prev match {
-            case None => { head = node.next }
             case Some(x) => { x.next = node.next }
+            case None => { top = node.next }
         }
-        node.next match {
-            case Some(x) => { x.prev = node.prev }
+        if (node.next.isDefined) {
+            node.next.get.prev = node.prev
         }
 
         return node.data
     }
 
-    def foreach[U](f: LinkedNode[A] => U) {
-        var node: Option[LinkedNode[A]] = head
-        while (node != None) {
-            node match {
-                case Some(x) => f(x)
+    def iterator = new Iterator[LinkedNode[A]] {
+        var nextLink: Option[LinkedNode[A]] = top
+
+        def hasNext = nextLink.isDefined
+
+        def next: LinkedNode[A] = {
+            nextLink match {
+                case Some(x) => {
+                    nextLink = x.next
+                    x
+                }
+                case None => null
             }
-            node = node.next
         }
     }
 
     def dequeue() : Option[LinkedNode[A]] = {
-        return head match {
-            case None => None
+        return top match {
             case Some(x) => {
                 remove(x)
                 Some(x)
             }
+            case None => None
         }
     }
 }
