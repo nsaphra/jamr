c3c810c9e3e3ee28404a706040774596eefd68af
N. Saphra <nsaphra@gmail.com>
2014-07-29 10:50:53 +0200

    EdgeQueue compiles

--- src/JointDecoder/EdgeQueue.scala
+++ src/JointDecoder/EdgeQueue.scala
@@ -2,9 +2,9 @@ package edu.cmu.lti.nlp.amr.JointDecoder
 import edu.cmu.lti.nlp.amr._
 import edu.cmu.lti.nlp.amr.FastFeatureVector._
 
-import scala.collection.mutable.Map
-import scala.collection.mutable.Set
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashSet
+import scala.collection.mutable.Map
 
 class EdgeQueue(nodeWeights: Array[Double],
                 edgeWeights: Array[Array[Double]],
@@ -28,13 +28,9 @@ class EdgeQueue(nodeWeights: Array[Double],
         (x: Fragment, y: Fragment) => x.totalWeight > y.totalWeight
     )
 
-/*
     // Map of nodes to the Edge LinkedNodes connected to them
-    var node2links: Map[Int, Set[Link]] = {
-        // TODO make it an array like everything else
-        val m: Map[Int, Set[Link]] = nodeWeights.zipWithIndex map { case (w, node) =>
-            (node -> Set[Link]())
-        }
+    var node2links: Array[HashSet[Link]] = {
+        val m: Array[HashSet[Link]] = nodeWeights map { _ => new HashSet[Link] }
         for (link <- queue) {
             m(link.data.edge.node1) += link
             m(link.data.edge.node2) += link
@@ -54,26 +50,26 @@ class EdgeQueue(nodeWeights: Array[Double],
         }
     }
 
-    def adjustFragmentWeights(node: Int) : Array[(Int, Int)] = {
+    def adjustFragmentWeights(node: Int) : ArrayBuffer[Edge] = {
         // Subtracts node weight from its edges
         // @return A list of fragments that can be added to graph immediately
-        var edges_to_confirm = new Array[(Int, Int)]
+        var edges_to_confirm = new ArrayBuffer[Edge]
 
         if (nodeWeights(node) == 0) {
             return edges_to_confirm
         }
 
-        val old_links: Set[Link] = node2links(node)
-        node2links(node) = new Set[Link]
+        val old_links: HashSet[Link] = node2links(node)
+        node2links(node) = new HashSet[Link]
         for (link <- old_links) {
             queue.remove(link)
         }
 
         var new_frags = new ArrayBuffer[Fragment]
         for (link <- old_links) {
-            val frag = link.data
-            frag.totalWeight -= node.weight
-            frag.nodesWeighed -= 1
+            val frag = Fragment(link.data.edge,
+                                link.data.totalWeight - nodeWeights(node),
+                                link.data.nodesWeighed - 1)
 
             // If an edge is connecting two nodes already in the graph,
             // we decide to include or toss it based on whether the weight
@@ -81,7 +77,7 @@ class EdgeQueue(nodeWeights: Array[Double],
             if (frag.nodesWeighed != 0) {
                 new_frags.append(frag)
             } else if (frag.totalWeight > 0) {
-                edges_to_confirm.append((frag.edge.node1, frag.edge.node2))
+                edges_to_confirm.append(frag.edge)
             }
         }
 
@@ -94,18 +90,17 @@ class EdgeQueue(nodeWeights: Array[Double],
         return edges_to_confirm
     }
 
-    def nodeAdded(node: Int) : List[Fragment] = {
+    def nodeAdded(node: Int) : ArrayBuffer[Edge] = {
         for (enemy <- eliminatedNodes(node)) {
             removeAllEdges(enemy)
         }
         return adjustFragmentWeights(node)
     }
 
-    def dequeue() : Option[(Int, Int)] = {
-        return queue.head match {
-            case None => None
+    def dequeue() : Option[Edge] = {
+        return queue.top match {
             case Some(link) => Some(remove(link).edge)
+            case None => None
         }
     }
-*/
 }
